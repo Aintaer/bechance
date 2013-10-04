684e1f89721b612956877310f2e3a3ddc09abaa0
Brian McAllister <brian@brianmcallister.com>
2013-10-04 14:51:18 -0400

    Re-add the progress bar.

--- js/index.js
+++ js/index.js
@@ -6,19 +6,24 @@ require({
 		'three': {exports: "THREE"},
 		'globe': {deps: ['three'], exports: "DAT"}
 	}
-}, ['be', 'lib/world', 'views/global-header'],
-function(be, world, GlobalHeader) {
+}, ['be', 'lib/world', 'views/global-header', 'views/progress-bar'],
+function(be, world, GlobalHeader, ProgressBar) {
 	'use strict';
 
 	var searchTerm = 'nsfw';
 	var header = new GlobalHeader();
 	header.on('submitted', function (value) { searchTerm = value; doSearch(searchTerm); });
+  
+  var progress = new ProgressBar()
 
 	be("M55FPXPyfvChqq8GQ1TBopL8fH4cpCyd");
 
 	function doSearch (term) {
+    progress.start()
+    
 		be.project.search(term)
 		.then(function(data) {
+      progress.finish()
 			console.log('results', data);
 		});
 	}
