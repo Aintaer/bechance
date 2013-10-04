13aeeffe94ae3d59f641264d3f93a1edd639834b
Brian McAllister <brian@brianmcallister.com>
2013-10-04 11:15:59 -0400

    Add Sass files.

---
+++ config.rb
@@ -0,0 +1,9 @@
+asset_path = '/'
+
+http_path  = '/'
+css_dir    = 'css'
+images_dir = 'img'
+sass_dir   = 'sass'
+
+output_style = (environment == :production) ? :compressed : :expanded
+relative_assets = true
