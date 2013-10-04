6556ab0cc52a2cd99f83abea413d1e68e8dcb58a
Alex Lee <aintaer@aintaer.com>
2013-10-03 22:11:09 -0400

    Adding and using be.js

--- js/index.js
+++ js/index.js
@@ -3,11 +3,21 @@ require({
 	shim: {
 		'three': {exports: "THREE"}
 	}
-}, ['three'], function(THREE) {
+}, ['be', 'three'], function(be, THREE) {
 	'use strict';
 
 	var container = document.createElement('DIV');
 	document.body.appendChild(container);
 
 	container.textContent = "NO JQUERY EVAR";
+
+	be("M55FPXPyfvChqq8GQ1TBopL8fH4cpCyd");
+
+	be.project.search('nsfw')
+	.then(function(data) {
+		container.textContent =
+		data.projects.map(function(project) {
+			return project.name;
+		}).join(', ');
+	});
 });
